03fc55c2c955f4d71dbcec9cc1d2e7d8da005b64
David Leatham <david.leatham@bakerhughes.com>
2020-03-05 15:32:59 -0800

    Minor UI enhancements

---
+++ ProcDumpWrapper/Converters/BoolToColorConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace ProcDumpWrapper.Converters
+{
+    public class BoolToColorConverter : IValueConverter
+    {
+        public Brush TrueBrush { get; set; } = new SolidColorBrush(Colors.Green);
+        public Brush FalseBrush { get; set; } = new SolidColorBrush(Colors.DarkSlateGray);
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool myValue)
+            {
+                return myValue ? TrueBrush : FalseBrush;
+            }
+
+            return FalseBrush;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
